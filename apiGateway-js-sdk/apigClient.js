2dac2d301240c67407c1afdc01abd733c967b453
Abhi Lad <42882493+abhilad1009@users.noreply.github.com>
2023-03-28 10:58:15 -0400

    Add files via upload

--- apiGateway-js-sdk/apigClient.js
+++ apiGateway-js-sdk/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://9d7a565pv6.execute-api.us-east-1.amazonaws.com/test0';
+    var invokeUrl = 'https://zlv5ibeslg.execute-api.us-east-1.amazonaws.com/test0';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
